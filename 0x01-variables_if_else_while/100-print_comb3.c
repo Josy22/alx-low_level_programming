f152fffbb31fed9b8581bd1984491b2b454fc7dc
Josy22 <josyphina92@gmail.com>
2022-10-17 15:41:23 -0700

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,15 @@ int main(void)
 	int ones = '0';
 	int tens = '0';
 
-	for (tens = '0'; tens <= '0'; tens++)/*prints tens digit*/
+	for (tens = '0'; tens <= '0'; tens++) /*prints tens digit*/
 	{
-		for (ones = '0'; ones <= '0'; ones++)/*prints ones digit*/
+		for (ones = '0'; ones <= '0'; ones++) /*prints ones digit*/
 		{
-			If (!(ones == tens)  || (tens  ones))/*eliminates repitition*/
+			if (!(ones == tens)  || (tens > ones)) /*eliminates repitition*/
 			{
 				putchar(tens);
 				putchar(ones);
-				If (!(ones == '0' && tens == '0'))/*adds comma and space*/
+				if (!(ones == '0' && tens == '0')) /*adds comma and space*/
 				{
 					putchar(',');
 					putchar(' ');
