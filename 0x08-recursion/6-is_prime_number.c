573f83568526785e5675d06a2bbccf1df56fd526
Josy22 <josyphina92@gmail.com>
2022-11-09 14:25:57 -0800

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,9 +8,11 @@
  */
 int prime2(int a, int b)
 {
-	if (a == b)
+	if (a == b - 1)
 		return (1);
 	else if (a % b == 0)
+		return (0);
+	if (a % b != 0)
 		return (0);
 	return (0);
 	return (prime2(a, b + 1));
